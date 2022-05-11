42e858c64f5e2fef6535f07f0862d8636e0c1145
vidushalakshan <vidushalakshan7890@gmail.com>
2022-05-11 21:26:10 +0530

    updated right &  left & jump

---
+++ assests/js/js.js
@@ -0,0 +1,26 @@
+var counter = 0;
+var alive = true;
+var CharacterMin;
+var CharcterMax;
+var CharcterLeft = 0;
+
+$(document).keydown(function (e) {
+    CharacterMin = $("#character").position().left;
+
+    if (e.keyCode == 32 && alive) {
+        $("#character").addclass("animate");
+        setTimeout(function () {
+            $("#character").removeclass("animate");
+        }, 400);
+    } else if (e.keyCode == 39 && alive) {
+        CharcterLeft += 10;
+        $("#character").css({
+            left: CharcterLeft + "px",
+        });
+    } else if (e.keyCode == 37 && alive && CharacterMin >= 0) {
+        CharcterLeft -= 10;
+        $("#character").css({
+            left: CharcterLeft + "px"
+        });
+    }
+});
